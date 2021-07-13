2efafbb32344678695b1c722d9dfaef4cd08afb0
CSharp317 <40783858+CSharp317@users.noreply.github.com>
2021-07-12 20:36:34 -0400

    Create LoginController.cs

---
+++ WindowsIdentityFoundation/LoginController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace RelyingParty1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            FederatedAuthentication.WSFederationAuthenticationModule.RedirectToIdentityProvider("customsts.dev", "http://localhost:26756/user", true);
+            return View();
+        }
+    }
